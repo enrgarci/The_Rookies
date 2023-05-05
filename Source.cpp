5d74743dd71968757b9f5315027709c6cb8529a6
andri <97804237+andriana2@users.noreply.github.com>
2023-05-05 19:16:07 +0200

    source con menú

--- Source.cpp
+++ Source.cpp
@@ -2,28 +2,81 @@
 #include "ETSIDI.h"
 #include "freeglut.h"
 #include "Interface.h"
+#include "Menu.h"
+#include "Boton.h"
+#include "Window.h"
+#include <cstdlib>
 
+Window window;
 Interface interfaz;
+Menu menus;
+
 
 void onDraw() 
 {
-    interfaz.drawMovement();
+	switch (menus.getEstado())
+	{
+	case (Menu::INICIO): //1
+		menus.drawinicio();
+		break;
+
+	case (Menu::DEPASO):
+		menus.drawdepaso();
+		break;
+	case (Menu::INSTRUCIONES):
+		menus.drawinstrucciones();
+		break;
+	case (Menu::OPCION):
+		menus.drawopciones();
+		break;
+
+	case (Menu::JUEGO1VS1)://5
+		interfaz.drawBoard(menus.getEstadoSkin());
+		interfaz.drawPieces(menus.getEstadoSkin());
+		interfaz.drawMovement(menus.getEstadoSkin());
+
+
+		break;
+	case (Menu::JUEGO1VSIA):
+
+		break;
+	case (Menu::FINGANADO):
+
+		break;
+	case (Menu::FINPERDIDO):
+
+		break;
+	case (Menu::TABLAS):
+
+		break;
+	default: std::cout << "Ha fallado el source ventana";
+		break;
+	}
+
+
+	//glClear(GL_COLOR_BUFFER_BIT);
+
     glutSwapBuffers();
 }
 
 void onReshape(int w, int h) 
 {
-    interfaz.reshape(w, h);
+    
+	window.reshape(w, h);
 }
 
 void onMouse(int button, int state, int x, int y) 
 {
     interfaz.mouseBoard(button, state, x, y);
+	system("cls");
+	menus.botonVentana(button, state, x, y);
+
 }
 
 void onKeyboard(unsigned char key, int x, int y) 
 {
     interfaz.keyboardFullscreen(key, x, y);
+	menus.keyboardVentana(key, x, y);
 }
 
 int main(int argc, char** argv) 
@@ -36,6 +89,7 @@ int main(int argc, char** argv)
 
     // Registrar las funciones de callback
     interfaz.init();
+	menus.menusIni();
     glutDisplayFunc(onDraw);
     glutReshapeFunc(onReshape);
     glutMouseFunc(onMouse);
