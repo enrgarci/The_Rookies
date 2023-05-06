113e1a8c253c67558d5f19beb6567d7ac9efc3e1
Rubén <80412404+rgladron@users.noreply.github.com>
2023-05-06 09:33:58 +0200

    New "submenu" when pause is pressed

--- src/Menu.h
+++ src/Menu.h
@@ -16,17 +16,21 @@ private:
 	Boton instrucciones, opciones, juego1vs1, juego1vsia, creditos, Logo, depaso, exit;
 	//Buttons in options
 	Boton homefromBoton, classic, pvsz, sw, pk, visualiza;
-
+	//Buttons in submenu
+	Boton BotonGuardar, BotonCargar, Boton3min, Boton5min, Boton10min, BotonReanudar, BotonMenuPpal;
+	int aumento=1;
 
 public:
 	enum ventana {
-		INICIO = 1, DEPASO, INSTRUCIONES, OPCION,
-		JUEGO1VS1, JUEGO1VSIA, FINGANADO, FINPERDIDO, TABLAS, CREDITOS
+		INICIO = 1,       DEPASO,   INSTRUCIONES,     OPCION, 
+		JUEGO1VS1,    JUEGO1VSIA,   FINGANADO,    FINPERDIDO, 
+		TABLAS,         CREDITOS,   SUBMENU					  
 	};//en la ventanaDEPASO estan las instrucciones juego1vs1 juego1vsia y opcion
 
 	//enum skin { normal = 0, classic = 1, pvsz, sw };
 
 	int getEstado() { return Estado; }
+	int* returnEstado() { return &Estado; }
 	void menusIni();
 	void drawinicio(void);
 	void drawdepaso(void);
@@ -35,6 +39,7 @@ public:
 	void drawopcionclassic(void);
 	void drawopcionpvsz(void);
 	void drawopcionsw(void);
+	void drawSubmenu(void);
 	void reshape(int w, int h);
 	//void cambioVentana();
 	void poligonoVistaImagen(void);
