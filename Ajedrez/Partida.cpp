f6af4848184b5e7fa2f6dcfed2d30b10f7d3325a
enrgarci <enrgarci@student.42.fr>
2023-05-08 13:26:40 +0200

    formating

--- Ajedrez/Partida.cpp
+++ Ajedrez/Partida.cpp
@@ -4,9 +4,9 @@
 #include "Clock.h"
 
 Partida::Partida(string w_player, string b_player,
-					double initial_time,
-					double increment,
-					 string init_pos)
+				 double initial_time,
+				 double increment,
+				 string init_pos)
 {
 	T = new Tablero(*this, init_pos);
 	m_w_player = w_player;
@@ -14,20 +14,20 @@ Partida::Partida(string w_player, string b_player,
 	positions.push_back(init_pos);
 	m_w_clock = new ChessClock(initial_time, increment);
 	m_b_clock = new ChessClock(initial_time, increment);
-	for(auto c : T->m_casilla)
+	for (auto c : T->m_casilla)
 		c->m_parent_board = T;
 }
 
 Partida::Partida(std::fstream &file)
 {
-	string raw,w_p, b_p;
+	string raw, w_p, b_p;
 	float start_t, increment;
 	vector<FEN> my_pos;
 	cout << "Reading from file..." << endl;
 	while (getline(file, raw))
 	{
 		bool done = false;
-		if (raw.find("White") != string::npos) 
+		if (raw.find("White") != string::npos)
 		{
 			int b, end, len;
 			b = raw.find_first_of("\"") + 1;
@@ -36,7 +36,7 @@ Partida::Partida(std::fstream &file)
 			w_p = raw.substr(b, end - b);
 			done = true;
 		}
-		else if (raw.find("Black") != string::npos) 
+		else if (raw.find("Black") != string::npos)
 		{
 			int b, end, len;
 			b = raw.find_first_of("\"") + 1;
@@ -45,7 +45,7 @@ Partida::Partida(std::fstream &file)
 			b_p = raw.substr(b, end - b);
 			done = true;
 		}
-		else if (raw.find("Start_Time") != string::npos) 
+		else if (raw.find("Start_Time") != string::npos)
 		{
 			int b, end, len;
 			b = raw.find_first_of("\"") + 1;
@@ -54,7 +54,7 @@ Partida::Partida(std::fstream &file)
 			start_t = std::stoi(raw.substr(b, end - b));
 			done = true;
 		}
-		else if (raw.find("Increment") != string::npos) 
+		else if (raw.find("Increment") != string::npos)
 		{
 			int b, end, len;
 			b = raw.find_first_of("\"") + 1;
@@ -63,7 +63,8 @@ Partida::Partida(std::fstream &file)
 			increment = std::stoi(raw.substr(b, end - b));
 			done = true;
 		}
-		else if(!done) my_pos.push_back(raw);
+		else if (!done)
+			my_pos.push_back(raw);
 	}
 	file.close();
 	m_w_player = w_p;
@@ -81,7 +82,7 @@ Partida::~Partida()
 	delete Tablero::s_knight, Tablero::s_knight = nullptr;
 	delete Tablero::s_rook, Tablero::s_rook = nullptr;
 	delete Tablero::s_pawn, Tablero::s_pawn = nullptr;
-	delete Tablero::s_bishop,Tablero::s_bishop = nullptr;
+	delete Tablero::s_bishop, Tablero::s_bishop = nullptr;
 	delete Tablero::s_empty, Tablero::s_empty = nullptr;
 	delete T;
 	delete m_w_clock;
@@ -95,32 +96,34 @@ Tablero &Partida::getBoard()
 
 string Partida::getPlayer(int col)
 {
-	if (col == 2) return(m_b_player);
-	if (col == 1) return(m_w_player);
+	if (col == 2)
+		return (m_b_player);
+	if (col == 1)
+		return (m_w_player);
 	return m_w_player + "/" + m_b_player;
 }
 
 void Partida::play_back()
 {
-	if (current_pos >= 1) 
+	if (current_pos >= 1)
 	{
 		current_pos--;
 		delete T;
 		T = new Tablero(*this, positions.at(current_pos));
-		for(auto c : T->m_casilla)
-		c->m_parent_board = T;
+		for (auto c : T->m_casilla)
+			c->m_parent_board = T;
 	}
 }
 
 void Partida::play_forward()
 {
-	if (current_pos < positions.size() - 1) 
+	if (current_pos < positions.size() - 1)
 	{
 		current_pos++;
 		delete T;
-		T = new Tablero(*this,positions.at(current_pos));
-		for(auto c : T->m_casilla)
-		c->m_parent_board = T;
+		T = new Tablero(*this, positions.at(current_pos));
+		for (auto c : T->m_casilla)
+			c->m_parent_board = T;
 	}
 }
 
@@ -128,22 +131,23 @@ void Partida::play_last()
 {
 	current_pos = positions.size() - 1;
 	delete T;
-	T = new Tablero(*this,positions.at(current_pos));
-	for(auto c : T->m_casilla)
+	T = new Tablero(*this, positions.at(current_pos));
+	for (auto c : T->m_casilla)
 		c->m_parent_board = T;
 }
 void Partida::play_first()
 {
 	current_pos = 0;
 	delete T;
-	T = new Tablero(*this,positions.at(current_pos));
-	for(auto c : T->m_casilla)
+	T = new Tablero(*this, positions.at(current_pos));
+	for (auto c : T->m_casilla)
 		c->m_parent_board = T;
 }
 
 void Partida::add_pos()
 {
-	if (current_pos != positions.size() - 1) return;
+	if (current_pos != positions.size() - 1)
+		return;
 	positions.push_back((*T).get_fen());
 	current_pos = positions.size() - 1;
 }
@@ -151,11 +155,12 @@ void Partida::add_pos()
 void Partida::undoMove()
 {
 	Tablero *p_T = T;
-	if (current_pos != positions.size() - 1) return;
+	if (current_pos != positions.size() - 1)
+		return;
 	positions.pop_back();
 	current_pos = positions.size() - 1;
-	T = new Tablero(*this,positions.back());
-	for(auto c : T->m_casilla)
+	T = new Tablero(*this, positions.back());
+	for (auto c : T->m_casilla)
 		c->m_parent_board = T;
 	delete p_T;
 }
@@ -168,24 +173,28 @@ void Partida::save(string directory, string name)
 	saveFile << "[Black \"" << m_b_player << "\"]  " << endl;
 	saveFile << "[Start_Time \"" << m_w_clock->getInitialTime() << "\"]  " << endl;
 	saveFile << "[Increment \"" << m_w_clock->getIncremet() << "\"]  " << endl;
-	for (auto s : positions) saveFile << s << endl;
+	for (auto s : positions)
+		saveFile << s << endl;
 	saveFile.close();
 }
 
-int	Partida::perf(Partida &p, int depth)
+int Partida::perf(Partida &p, int depth)
 {
-	if (depth == 0) return 1;
+	if (depth == 0)
+		return 1;
 	int nodes = 0;
 	static int special[4] = {};
 
 	for (int i = 0; i < BOARD_SIZE; i++)
 	{
-		if (p.T->get_cell(i).getFigura() == Vacio) continue;
-		if (p.T->get_cell(i).getColor() != p.T->get_turn()) continue;
+		if (p.T->get_cell(i).getFigura() == Vacio)
+			continue;
+		if (p.T->get_cell(i).getColor() != p.T->get_turn())
+			continue;
 		vector<int> T_moves = p.T->get_cell(i).getMoveList();
 		for (auto move : T_moves)
 		{
-			special [p.T->do_move(i, move)]++;
+			special[p.T->do_move(i, move)]++;
 			nodes += perf(p, depth - 1);
 			p.undoMove();
 		}
@@ -195,14 +204,18 @@ int	Partida::perf(Partida &p, int depth)
 
 float Partida::getColorClock(color col)
 {
-	if (!col) return 0;
-	if(col == Blanco) return m_w_clock->updateChessClock();
+	if (!col)
+		return 0;
+	if (col == Blanco)
+		return m_w_clock->updateChessClock();
 	return m_b_clock->updateChessClock();
 }
 
 void Partida::startColorClock(color col)
 {
-	if (!col) return;
-	if(col == Blanco) return m_w_clock->setOrigin();
+	if (!col)
+		return;
+	if (col == Blanco)
+		return m_w_clock->setOrigin();
 	return m_b_clock->setOrigin();
 }
